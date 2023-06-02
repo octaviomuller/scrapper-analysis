f473e360948ccc7704d14a58ab634c5a25420fbb
leoruas <leonardo.ruas@going2.com.br>
2023-06-02 04:08:17 -0300

    chore: start website maximized

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # TODO: Adicionar extração de palavra chave (Tensor Flow?)
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from jinja2 import Environment, FileSystemLoader
 import os
@@ -12,7 +13,11 @@ from fonts import get_font_set
 from products import get_products
 from template import open_template
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
 driver.get('https://www.kabum.com.br/ofertas/megamaio')
 
 colors = get_colors(driver)
